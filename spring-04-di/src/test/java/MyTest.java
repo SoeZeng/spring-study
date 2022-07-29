0d561d7a5608f95cb32bc99024308a665cdee639
SoeZeng <zengyishu213@163.com>
2022-07-29 22:59:00 +0800

    kuang_10

--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -1,6 +1,10 @@
 import com.kuang.pojo.Student;
+import com.kuang.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.naming.Context;
 
 public class MyTest {
 
@@ -13,5 +17,16 @@ public class MyTest {
         hobbies=[听歌, 敲代码, 看电影], card={身份证=123456123456781234, 银行卡=1231231231231231231},
         games=[LOL, COC, BOB], wife='null', info={password=12345, url=女, driver=20220714, username=Tina}}
          */
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+//        User user = (User) context.getBean("user");
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+        User user2 = context.getBean("user2", User.class);
+        System.out.println(user2);
+
     }
 }
